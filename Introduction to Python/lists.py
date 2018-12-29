f89ac5274e99be5a285170aebbcedfae353c9bfa
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-12-29 13:32:46 -0500

    Done lists

--- Introduction to Python/lists.py
+++ Introduction to Python/lists.py
@@ -1,1 +1,44 @@
 # Lists
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+# Use slicing to create downstairs
+downstairs = areas[:6]
+
+# Use slicing to create upstairs
+upstairs = areas[-4:]
+
+# Print out downstairs and upstairs
+print(downstairs, upstairs)
+
+# Creating an explicit copy
+areas_copy = areas[:]
