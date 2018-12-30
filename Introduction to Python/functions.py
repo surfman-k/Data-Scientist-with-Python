7a8d097d6cb8d608363b9d019370e867e688e10b
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-12-30 14:12:10 -0500

    Done Packages!

--- Introduction to Python/functions.py
+++ Introduction to Python/functions.py
@@ -51,4 +51,67 @@ print(areas.index(20.0))
 # Print out how often 9.50 appears in areas
 print(areas.count(9.50))
 
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+
+##################################################
+
+# Packages
+
+# Numpy to efficiently work with arrays
+# Matplotlib for data visualization
+# Scikit-learn for machine learning
+
+import numpy
+numpy.array([1,2,3])
+
+# OR
+
+import numpy as np
+np.array([1,2,3])
+
+# OR
+
+from numpy import array # Unclear better use one of the two examples above
+array([1,2,3])
+
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * math.pi * r
+
+# Calculate A
+A = math.pi * r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = r * radians(12)
+
+# Print out dist
+print(dist)
