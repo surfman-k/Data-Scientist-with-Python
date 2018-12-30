e2b5cb32428d36fab07e552a6b51ad58862620b8
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-12-30 14:01:02 -0500

    Done Methods

--- Introduction to Python/functions.py
+++ Introduction to Python/functions.py
@@ -25,3 +25,30 @@ full_sorted = sorted(full, reverse=True)
 
 # Print out full_sorted
 print(full_sorted)
+
+##################################################
+
+# Methods
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place, place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+
