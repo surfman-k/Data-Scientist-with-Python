9f872b982171c37d2b58cca0865c024ed48781e2
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-12-31 14:22:11 -0500

    Completed 2D arrays

--- Introduction to Python/numpy.py
+++ Introduction to Python/numpy.py
@@ -66,3 +66,47 @@ print(np_weight_lb[50])
 
 # Print out sub-array of np_height: index 100 up to and including index 110
 print(np_height_in[100:111])
+
+
+# 2D NumPy Arrays
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
+
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
