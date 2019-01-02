f95042a6e61216811ce6a9f513d55a1f2d17a13d
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-02 17:15:57 -0500

    Comparison Operators

---
+++ Intermediate Python for Data Science/logic-control-filter.py
@@ -0,0 +1,36 @@
+# Logic, Control Flow, and Filtering
+
+# Comparison Operators
+
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+-5 * 15 != 75
+
+# Comparison of strings
+"pyscript" == "PyScript"
+
+# Compare a boolean with an integer
+True == 1
+
+# Comparison of integers
+x = -3 * 6
+print(x >= -10)
+
+# Comparison of strings
+y = "test"
+print('test' <= y)
+
+# Comparison of booleans
+print(True > False)
+
+
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house < your_house)
