7eda90091c7ce618e4867cc2e893b819adfac921
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-02 13:12:27 -0500

    Histograms

--- Intermediate Python for Data Science/matplotlib.py
+++ Intermediate Python for Data Science/matplotlib.py
@@ -29,3 +29,60 @@ plt.xscale('log')
 
 # Show plot
 plt.show()
+
+
+# Histograms 
+
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp, bins=5)
+
+# Show and clean up plot
+plt.show()
+plt.clf() # clear figure
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins=20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp, bins=15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950, bins=15)
+
+# Show and clear plot again
+plt.show()
+plt.clf()
+
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
