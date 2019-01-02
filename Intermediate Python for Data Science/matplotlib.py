3bb04b3130daa4136ab5226cf23ec23f011535e7
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-01 21:31:16 -0500

    Basic line and scatter plots

--- Intermediate Python for Data Science/matplotlib.py
+++ Intermediate Python for Data Science/matplotlib.py
@@ -1,1 +1,31 @@
 # Matplotlib
+
+# Print the last item from year and pop
+print(year[-1], pop[-1])
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap[-1], life_exp[-1])
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
+
+# Display the plot
+plt.show()
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
