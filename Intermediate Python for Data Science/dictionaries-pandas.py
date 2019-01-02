f690ba28151dd8930477b359375e009450ea4121
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-02 16:59:41 -0500

    Pandas Part 2

--- Intermediate Python for Data Science/dictionaries-pandas.py
+++ Intermediate Python for Data Science/dictionaries-pandas.py
@@ -113,3 +113,35 @@ cars = pd.read_csv('cars.csv', index_col = 0)
 
 # Print out cars
 print(cars)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country','drives_right']])
+
+# Print out first 3 observations
+print(cars[:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
+
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+
+# Print out drives_right value of Morocco
+print(cars.loc[['MOR'],['drives_right']])
+
+# Print sub-DataFrame
+print(cars.loc[['RU', 'MOR'],['country', 'drives_right']])
