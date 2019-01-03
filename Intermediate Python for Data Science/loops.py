e3540b12993425ae3d619fd5875ce66c80945bd0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-03 17:35:24 -0500

    Looping over Pandas and adding columns with loops and apply()

--- Intermediate Python for Data Science/loops.py
+++ Intermediate Python for Data Science/loops.py
@@ -49,3 +49,45 @@ house = [["hallway", 11.25],
 for list in house:
     print('the ' + str(list[0]) + ' is ' + str(list[1]) + ' sqm')
 
+
+# Looping Data Structures
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for k, v in europe.items():
+    print('the capital of ' + k + ' is ' + v)
+
+# For loop over np_height
+for x in np_height:
+    print(str(x) + ' inches')
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball):
+    print(x)
+
+
+# Looping over Pandas
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for lab, row in cars.iterrows():
+    print(lab)
+    print(row)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(row['country'] + ': ' + str(row['cars_per_cap']))
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows():
+    cars.loc[lab, 'COUNTRY'] = str(row['country']).upper()
+
+# Use .apply(str.upper)
+cars["COUNTRY"] = cars["country"].apply(str.upper)
+
