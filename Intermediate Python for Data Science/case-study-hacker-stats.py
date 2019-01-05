1526776101a78a8a727d6a7c39f8267d6e5141de
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-04 23:55:45 -0500

    Code cleanup and setup game logic

--- Intermediate Python for Data Science/case-study-hacker-stats.py
+++ Intermediate Python for Data Science/case-study-hacker-stats.py
@@ -18,12 +18,25 @@ np.random.seed(123)
 # Generate and print random float
 print(np.random.rand())
 
-# Import numpy and set seed
-import numpy as np
-np.random.seed(123)
-
 # Use randint() to simulate a dice
 print(np.random.randint(1,7))
 
 # Use randint() again
 print(np.random.randint(1,7))
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice > 2 and dice < 6 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice, step)
