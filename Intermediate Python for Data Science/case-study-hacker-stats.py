427892f143c6dc7c60c1c583cf787274a55b0f95
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-05 00:21:06 -0500

    Visualizing Runs

--- Intermediate Python for Data Science/case-study-hacker-stats.py
+++ Intermediate Python for Data Science/case-study-hacker-stats.py
@@ -66,3 +66,42 @@ for x in range(100) :
 # Print random_walk
 print(random_walk)
 
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
+
+
+
+# Visualizing multiple runs
+
+# initialize and populate all_walks
+all_walks = []
+for i in range(10) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to Numpy array: np_aw
+np_aw = np.array(all_walks)
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
