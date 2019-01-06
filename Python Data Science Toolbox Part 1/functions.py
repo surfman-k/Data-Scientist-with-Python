1902083c16757beb99b14930e41354bc7be96b27
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-06 01:32:07 -0500

    Functions

--- Python Data Science Toolbox Part 1/functions.py
+++ Python Data Science Toolbox Part 1/functions.py
@@ -1,1 +1,69 @@
 # Building Pythong Functions
+
+# No Parameters
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
+
+
+# Single Parameter
+# Define shout with the parameter, word
+def shout2(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout2('congratulations')
+
+
+# Returning Values
+# Define shout with the parameter, word
+def shout3(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Replace print with return
+    return(shout_word)
+
+# Pass 'congratulations' to shout: yell
+yell = shout3('congratulations')
+
+# Print yell
+print(yell)
+
+
+# Functions with multiple params and return values
+# Define shout with parameters word1 and word2
+def shout4(word1, word2):
+    """Concatenate strings with three exclamation marks"""
+    # Concatenate word1 with '!!!': shout1
+    shout1 = word1 + '!!!'
+    
+    # Concatenate word2 with '!!!': shout2
+    shout2 = word2 + '!!!'
+    
+    # Concatenate shout1 with shout2: new_shout
+    new_shout = shout1 + shout2
+
+    # Return new_shout
+    return new_shout
+
+# Pass 'congratulations' and 'you' to shout(): yell
+yell = shout4('congratulations', 'you')
+
+# Print yell
+print(yell)
+
