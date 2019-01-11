64da9a26f6443120aab28f013d163448cd1036aa
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-11 16:24:46 -0500

    Reading Txt files in Python

--- Importing Data in Pythong Part 1/importing-data-flat-files.py
+++ Importing Data in Pythong Part 1/importing-data-flat-files.py
@@ -2,3 +2,29 @@
 ## Importing data from flat files ##
 ####################################
 
+
+# Open a file: file
+file = open('moby_dick.txt', 'r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
+
+
+# Can use 'with' as a context manager so we don't have to close the file  <--- Best Practice
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+
+
