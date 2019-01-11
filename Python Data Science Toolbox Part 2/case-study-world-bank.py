6c7521417de52746964debbeb6772b0ea79c4da4
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-11 15:34:28 -0500

    Data setup

--- Python Data Science Toolbox Part 2/case-study-world-bank.py
+++ Python Data Science Toolbox Part 2/case-study-world-bank.py
@@ -1,3 +1,76 @@
 ############################################################
 ## Case Study : World Bank's World Development Indicators ##
 ############################################################
+
+# Data on 217 world economies for 1960-2015
+# Indicators such as: Population, Electricity consumption, CO2 emissions, Literacy rates, Unemployment, Mortality Rates
+
+
+
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names, row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
+
+
+
+# Putting the above code into a function for reusability:
+
+# Define lists2dict()
+def lists2dict(list1, list2):
+    """Return a dictionary where list1 provides
+    the keys and list2 provides the values."""
+
+    # Zip lists: zipped_lists
+    zipped_lists = zip(list1, list2)
+
+    # Create a dictionary: rs_dict
+    rs_dict = dict(zipped_lists)
+
+    # Return the dictionary
+    return rs_dict
+
+# Call lists2dict: rs_fxn
+rs_fxn = lists2dict(feature_names, row_vals)
+
+# Print rs_fxn
+print(rs_fxn)
+
+
+
+########################
+## List Comprehension ##
+########################
+
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
+
+
+
+##############################
+## Turn it into a DataFrame ##
+##############################
+
+# Import the pandas package
+import pandas as pd
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Turn list of dicts into a DataFrame: df
+df = pd.DataFrame(list_of_dicts)
+
+# Print the head of the DataFrame
+print(df.head())
