6659b1174fe9489ae9731af293bf01d3b97680cb
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-16 16:37:22 -0500

    HTTP Requests with Python

--- Importing Data in Python Part 2/importing-files-from-the-web.py
+++ Importing Data in Python Part 2/importing-files-from-the-web.py
@@ -52,3 +52,49 @@ print(x1.keys())
 
 # Print the head of the first sheet (using its name, NOT its index)
 print(x1['1700'].head())
+
+
+###############################
+## HTTP Requests with Python ##
+###############################
+
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "http://www.datacamp.com/teach/documentation"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+
+
+##################################
+## Requests Package with Python ##
+##################################
+
+# Import package
+import requests
+
+# Specify the url: url
+url = "http://www.datacamp.com/teach/documentation"
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
