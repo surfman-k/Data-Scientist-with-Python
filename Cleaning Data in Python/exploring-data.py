3506f33dc1d8006d849bde45410858945318a021
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-18 20:22:57 -0500

    Data Cleaning

--- Cleaning Data in Python/exploring-data.py
+++ Cleaning Data in Python/exploring-data.py
@@ -1,3 +1,15 @@
 ####################
 ## Exploring Data ##
 ####################
+
+# Diagnosing Data that needs to be cleaned:
+
+# Common Data Problems:
+# -Inconsistent column names
+# -Missing data
+# -Outliers
+# -Duplicate Rows
+# -Untidy
+# -Need to process columns
+# -Column types can signal unexpected data values
+ 
