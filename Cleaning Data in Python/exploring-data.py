1d1631c983fde3a1494c742841490b9cf0d4fd69
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-19 23:25:38 -0500

    Visualizing the Data

--- Cleaning Data in Python/exploring-data.py
+++ Cleaning Data in Python/exploring-data.py
@@ -64,3 +64,51 @@ print(df['State'].value_counts(dropna=False))
 
 # Print the value counts for 'Site Fill'
 print(df['Site Fill'].value_counts(dropna=False))
+
+
+
+##########################
+## Visualizing the Data ##
+##########################
+
+### Histograms 
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Describe the column
+df['Existing Zoning Sqft'].describe()
+
+# Plot the histogram
+df['Existing Zoning Sqft'].plot(kind='hist', rot=70, logx=True, logy=True)
+
+# Display the histogram
+plt.show()
+
+
+### Boxplots
+
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create the boxplot
+df.boxplot(column='initial_cost', by='Borough', rot=90)
+
+# Display the plot
+plt.show()
+
+
+### Scatter Plots
+
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create and display the first scatter plot
+df.plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
+
+# Create and display the second scatter plot
+df_subset.plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
