243e5f812c3c35486847f16061389f1151bf09a0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-20 22:24:33 -0500

    Melting Data

--- Cleaning Data in Python/tidying-data-for-analysis.py
+++ Cleaning Data in Python/tidying-data-for-analysis.py
@@ -1,3 +1,12 @@
 ###############
 ## Tidy Data ##
 ###############
+
+# Print the head of airquality
+print(airquality.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'], var_name='measurement', value_name='reading')
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
