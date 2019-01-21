51e4cea463ea81f0fe6c237679e9670e07f71265
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-20 22:48:52 -0500

    Beyond Melting and Pivoting

--- Cleaning Data in Python/tidying-data-for-analysis.py
+++ Cleaning Data in Python/tidying-data-for-analysis.py
@@ -7,6 +7,8 @@
 # ● Rows represent individual observations
 # ● Observational units form tables
 
+
+
 ###############
 ## Melt Data ##
 ###############
@@ -19,6 +21,7 @@ airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'], var_name='measur
 
 # Print the head of airquality_melt
 print(airquality_melt.head())
+
 
 
 ################
@@ -50,7 +53,6 @@ print(airquality_pivot_reset.head())
 
 
 
-
 # Pivot table the airquality_dup: airquality_pivot
 airquality_pivot = airquality_dup.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
 
@@ -65,4 +67,39 @@ print(airquality_pivot.head())
 
 # Print the head of airquality
 print(airquality.head())
+
+
+
+######################################
+## Beyond Melting and Pivoting Data ##
+######################################
+
+# Melt tb: tb_melt
+tb_melt = pd.melt(frame=tb, id_vars=['country', 'year'])
+
+# Create the 'gender' column
+tb_melt['gender'] = tb_melt.variable.str[0]
+
+# Create the 'age_group' column
+tb_melt['age_group'] = tb_melt.variable.str[1:]
+
+# Print the head of tb_melt
+print(tb_melt.head())
+
+
+
+# Melt ebola: ebola_melt
+ebola_melt = pd.melt(ebola, id_vars=['Date', 'Day'], var_name='type_country', value_name='counts')
+
+# Create the 'str_split' column
+ebola_melt['str_split'] = ebola_melt.type_country.str.split('_')
+
+# Create the 'type' column
+ebola_melt['type'] = ebola_melt.str_split.str.get(0)
+
+# Create the 'country' column
+ebola_melt['country'] = ebola_melt.str_split.str.get(1)
+
+# Print the head of ebola_melt
+print(ebola_melt.head())
 
