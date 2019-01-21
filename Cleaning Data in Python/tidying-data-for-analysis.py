7c0d173c7c16f59522b632a516226fb8fd70ee0e
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-20 22:36:53 -0500

    Pivoting data

--- Cleaning Data in Python/tidying-data-for-analysis.py
+++ Cleaning Data in Python/tidying-data-for-analysis.py
@@ -2,6 +2,15 @@
 ## Tidy Data ##
 ###############
 
+# Principles of tidy data
+# ● Columns represent separate variables
+# ● Rows represent individual observations
+# ● Observational units form tables
+
+###############
+## Melt Data ##
+###############
+
 # Print the head of airquality
 print(airquality.head())
 
@@ -10,3 +19,50 @@ airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'], var_name='measur
 
 # Print the head of airquality_melt
 print(airquality_melt.head())
+
+
+################
+## Pivot Data ##
+################
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Pivot airquality_melt: airquality_pivot
+airquality_pivot = airquality_melt.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading')
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+
+
+# Print the index of airquality_pivot
+print(airquality_pivot.index)
+
+# Reset the index of airquality_pivot: airquality_pivot_reset
+airquality_pivot_reset = airquality_pivot.reset_index()
+
+# Print the new index of airquality_pivot_reset
+print(airquality_pivot_reset.index)
+
+# Print the head of airquality_pivot_reset
+print(airquality_pivot_reset.head())
+
+
+
+
+# Pivot table the airquality_dup: airquality_pivot
+airquality_pivot = airquality_dup.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
+
+# Print the head of airquality_pivot before reset_index
+print(airquality_pivot.head())
+
+# Reset the index of airquality_pivot
+airquality_pivot = airquality_pivot.reset_index()
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+# Print the head of airquality
+print(airquality.head())
+
