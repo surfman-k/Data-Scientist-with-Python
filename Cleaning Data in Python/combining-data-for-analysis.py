65ab1eb0299a3163a9605f067e84c545aa1f3975
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-21 23:04:33 -0500

    Concatenating many files

--- Cleaning Data in Python/combining-data-for-analysis.py
+++ Cleaning Data in Python/combining-data-for-analysis.py
@@ -25,3 +25,51 @@ print(ebola_tidy.shape)
 
 # Print the head of ebola_tidy
 print(ebola_tidy.head())
+
+
+
+##############################
+## Concatenating many files ##
+##############################
+
+# Import necessary modules
+import glob
+import pandas as pd
+
+# Write the pattern: pattern
+pattern = '*.csv'
+
+# Save all file matches: csv_files
+csv_files = glob.glob(pattern)
+
+# Print the file names
+print(csv_files)
+
+# Load the second file into a DataFrame: csv2
+csv2 = pd.read_csv(csv_files[1])
+
+# Print the head of csv2
+print(csv2.head())
+
+
+
+# Create an empty list: frames
+frames = []
+
+#  Iterate over csv_files
+for csv in csv_files:
+
+    #  Read csv into a DataFrame: df
+    df = pd.read_csv(csv)
+    
+    # Append df to frames
+    frames.append(df)
+
+# Concatenate frames into a single DataFrame: uber
+uber = pd.concat(frames)
+
+# Print the shape of uber
+print(uber.shape)
+
+# Print the head of uber
+print(uber.head())
