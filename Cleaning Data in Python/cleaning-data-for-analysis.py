94303580e7b46d956dbb2ac8a9dcf9bc2228cad2
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-22 17:30:21 -0500

    Converting Data types

--- Cleaning Data in Python/cleaning-data-for-analysis.py
+++ Cleaning Data in Python/cleaning-data-for-analysis.py
@@ -1,3 +1,21 @@
 ################
 ## Data types ##
 ################
+
+# Convert the sex column to type 'category'
+tips.sex = tips.sex.astype('category')
+
+# Convert the smoker column to type 'category'
+tips.smoker = tips.smoker.astype('category')
+
+# Print the info of tips
+print(tips.info())
+
+# Convert 'total_bill' to a numeric dtype
+tips['total_bill'] = pd.to_numeric(tips['total_bill'], errors='coerce')
+
+# Convert 'tip' to a numeric dtype
+tips['tip'] = pd.to_numeric(tips['tip'], errors='coerce')
+
+# Print the info of tips
+print(tips.info())
