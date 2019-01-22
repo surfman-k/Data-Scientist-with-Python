804850d9677813d4685032522a2eab3c3833a2ac
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-21 22:55:32 -0500

    Concatenating data

--- Cleaning Data in Python/combining-data-for-analysis.py
+++ Cleaning Data in Python/combining-data-for-analysis.py
@@ -1,3 +1,27 @@
 ########################
 ## Concatenating data ##
 ########################
+
+### By Row
+
+# Concatenate uber1, uber2, and uber3: row_concat
+row_concat = pd.concat([uber1, uber2, uber3])
+
+# Print the shape of row_concat
+print(row_concat.shape)
+
+# Print the head of row_concat
+print(row_concat.head())
+
+
+
+### By Column
+
+# Concatenate ebola_melt and status_country column-wise: ebola_tidy
+ebola_tidy = pd.concat([ebola_melt, status_country], axis=1)
+
+# Print the shape of ebola_tidy
+print(ebola_tidy.shape)
+
+# Print the head of ebola_tidy
+print(ebola_tidy.head())
