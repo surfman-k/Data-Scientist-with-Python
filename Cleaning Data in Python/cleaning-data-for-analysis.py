cbccf27e0be099d135616516d76a5e3fdc79f4f4
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-24 17:05:54 -0500

    Cleaning data using functions

--- Cleaning Data in Python/cleaning-data-for-analysis.py
+++ Cleaning Data in Python/cleaning-data-for-analysis.py
@@ -65,3 +65,42 @@ print(pattern2)
 # Write the third pattern
 pattern3 = bool(re.match(pattern='[A-Z]\w*', string='Australia'))
 print(pattern3)
+
+
+
+###################################
+## Using Functions to Clean Data ##
+###################################
+
+# Define recode_gender()
+def recode_gender(gender):
+
+    # Return 0 if gender is 'Female'
+    if gender == 'Male':
+        return 1
+    
+    # Return 1 if gender is 'Male'    
+    elif gender == 'Female':
+        return 0
+    
+    # Return np.nan    
+    else:
+        return np.nan
+
+# Apply the function to the sex column
+tips['recode'] = tips.sex.apply(recode_gender)
+
+# Print the first five rows of tips
+print(tips.head())
+
+
+
+
+# Write the lambda function using replace
+tips['total_dollar_replace'] = tips.total_dollar.apply(lambda x: x.replace('$', ''))
+
+# Write the lambda function using regular expressions
+tips['total_dollar_re'] = tips.total_dollar.apply(lambda x: re.findall('\d+\.\d+', x)[0])
+
+# Print the head of tips
+print(tips.head())
