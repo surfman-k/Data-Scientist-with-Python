b1e35a14d6565eae1d4ed240a835a60ae4538233
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-28 20:55:31 -0500

    Testing and assembling the data

--- Cleaning Data in Python/case-study-gapminder-foundation.py
+++ Cleaning Data in Python/case-study-gapminder-foundation.py
@@ -2,3 +2,58 @@
 ## Gapminder Foundation Case Study ##
 #####################################
 
+# Life Expectancy by Country and Year
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create the scatter plot
+g1800s.plot(kind='scatter', x='1800', y='1899')
+
+# Specify axis labels
+plt.xlabel('Life Expectancy by Country in 1800')
+plt.ylabel('Life Expectancy by Country in 1899')
+
+# Specify axis limits
+plt.xlim(20, 55)
+plt.ylim(20, 55)
+
+# Display the plot
+plt.show()
+
+
+
+### Verifying the data
+
+def check_null_or_valid(row_data):
+    """Function that takes a row of data,
+    drops all missing values,
+    and checks if all remaining values are greater than or equal to 0
+    """
+    no_na = row_data.dropna()
+    numeric = pd.to_numeric(no_na)
+    ge0 = numeric >= 0
+    return ge0
+
+# Check whether the first column is 'Life expectancy'
+assert g1800s.columns[0] == 'Life expectancy'
+
+# Check whether the values in the row are valid
+assert g1800s.iloc[:, 1:].apply(check_null_or_valid, axis=1).all().all()
+
+# Check that there is only one instance of each country
+assert g1800s['Life expectancy'].value_counts()[0] == 1
+
+
+
+### Assembling the data
+
+# Concatenate the DataFrames row-wise
+gapminder = pd.concat([g1800s, g1900s, g2000s])
+
+# Print the shape of gapminder
+print(gapminder.shape)
+
+# Print the head of gapminder
+print(gapminder.head())
+
