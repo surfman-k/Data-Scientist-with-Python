7d2b905cb1cfe3e1c3f52fcb4445820329ac600a
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-29 20:24:58 -0500

    Plotting with Pandas

--- pandas Foundations/data-ingestion-and-inspection.py
+++ pandas Foundations/data-ingestion-and-inspection.py
@@ -96,3 +96,41 @@ df2.to_csv(file_clean, index=False)
 df2.to_excel('file_clean.xlsx', index=False)
 
 
+
+##########################
+## Plotting with Pandas ##
+##########################
+
+# Create a plot with color='red'
+df.plot(c='red')
+
+# Add a title
+plt.title('Temperature in Austin')
+
+# Specify the x-axis label
+plt.xlabel('Hours since midnight August 1, 2010')
+
+# Specify the y-axis label
+plt.ylabel('Temperature (degrees F)')
+
+# Display the plot
+plt.show()
+
+
+# Plot all columns (default)
+df.plot()
+plt.show()
+
+# Plot all columns as subplots
+df.plot(subplots=True)
+plt.show()
+
+# Plot just the Dew Point data
+column_list1 = ['Dew Point (deg F)']
+df[column_list1].plot()
+plt.show()
+
+# Plot the Dew Point and Temperature data, but not the Pressure data
+column_list2 = ['Temperature (deg F)','Dew Point (deg F)']
+df[column_list2].plot()
+plt.show()
