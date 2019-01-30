fe31f1762af4bc9170df245a95ed6ad5a88906e0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-29 19:46:10 -0500

    Review of pandas

--- pandas Foundations/data-ingestion-and-inspection.py
+++ pandas Foundations/data-ingestion-and-inspection.py
@@ -1,3 +1,23 @@
 #################################
 ## Review of pandas DataFrames ##
 #################################
+
+# Import numpy
+import numpy as np
+
+# Create array of DataFrame values: np_vals
+np_vals = df.values
+
+# Create new array of base 10 logarithm values: np_vals_log10
+np_vals_log10 = np.log10(np_vals)
+
+# Create array of new DataFrame by passing df to np.log10(): df_log10
+df_log10 = np.log10(df)
+
+# Print original and new data containers
+[print(x, 'has type', type(eval(x))) for x in ['np_vals', 'np_vals_log10', 'df', 'df_log10']]
+
+# As a data scientist, you'll frequently interact with NumPy arrays, pandas Series, and pandas DataFrames, and you'll leverage a variety of NumPy and pandas methods to perform your desired computations. Understanding how NumPy and pandas work together will prove to be very useful.
+
+
+
