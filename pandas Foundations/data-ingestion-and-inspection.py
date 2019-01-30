e40d03ca8d0132f2bd62d7c17aa9079263b30180
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-29 20:13:58 -0500

    Importing and Exporting DataFrames

--- pandas Foundations/data-ingestion-and-inspection.py
+++ pandas Foundations/data-ingestion-and-inspection.py
@@ -57,3 +57,42 @@ df = pd.DataFrame(data)
 # Print the DataFrame
 print(df)
 
+
+
+########################################
+## Importing and Exporting DataFrames ##
+########################################
+
+# Read in the file: df1
+df1 = pd.read_csv(data_file)
+
+# Create a list of the new column labels: new_labels
+new_labels = ['year','population']
+
+# Read in the file, specifying the header and names parameters: df2
+df2 = pd.read_csv('/usr/local/share/datasets/world_population.csv', header=0, names=new_labels)
+
+# Print both the DataFrames
+print(df1)
+print(df2)
+
+
+# Read the raw file as-is: df1
+df1 = pd.read_csv(file_messy)
+
+# Print the output of df1.head()
+print(df1.head())
+
+# Read in the file with the correct parameters: df2
+df2 = pd.read_csv(file_messy, delimiter=' ', header=3, comment='#')
+
+# Print the output of df2.head()
+print(df2.head())
+
+# Save the cleaned up DataFrame to a CSV file without the index
+df2.to_csv(file_clean, index=False)
+
+# Save the cleaned up DataFrame to an excel file without the index
+df2.to_excel('file_clean.xlsx', index=False)
+
+
