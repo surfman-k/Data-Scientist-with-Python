d9aa9a06862fe3af68c199e4ddd58d328bcc4543
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-29 19:51:16 -0500

    Creating DataFrames from scratch

--- pandas Foundations/data-ingestion-and-inspection.py
+++ pandas Foundations/data-ingestion-and-inspection.py
@@ -20,4 +20,40 @@ df_log10 = np.log10(df)
 # As a data scientist, you'll frequently interact with NumPy arrays, pandas Series, and pandas DataFrames, and you'll leverage a variety of NumPy and pandas methods to perform your desired computations. Understanding how NumPy and pandas work together will prove to be very useful.
 
 
+######################################
+## Creating DataFrames from scratch ##
+######################################
+
+# Zip the 2 lists together into one list of (key,value) tuples: zipped
+zipped = list(zip(list_keys, list_values))
+
+# Inspect the list using print()
+print(zipped)
+
+# Build a dictionary with the zipped list: data
+data = dict(zipped)
+
+# Build and inspect a DataFrame from the dictionary: df
+df = pd.DataFrame(data)
+print(df)
+
+# Build a list of labels: list_labels
+list_labels = ['year', 'artist', 'song', 'chart weeks']
+
+# Assign the list of labels to the columns attribute: df.columns
+df.columns = list_labels
+
+
+
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
 
