1ad1f9c3c48e72a2f2dcf9581f9bfc0d06ae9251
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-01-31 11:14:00 -0500

    4 Types of visual analysis

--- pandas Foundations/exploratory-data-analysis.py
+++ pandas Foundations/exploratory-data-analysis.py
@@ -1,3 +1,69 @@
 ######################################
 ## Visual exploratory data analysis ##
 ######################################
+
+
+### Line plot
+
+# Create a list of y-axis column names: y_columns
+y_columns = ['AAPL', 'IBM']
+
+# Generate a line plot
+df.plot(x='Month', y=y_columns)
+
+# Add the title
+plt.title('Monthly stock prices')
+
+# Add the y-axis label
+plt.ylabel('Price ($US)')
+
+# Display the plot
+plt.show()
+
+
+
+### Scatter Plot
+
+# Generate a scatter plot
+df.plot(kind='scatter', x='hp', y='mpg', s=sizes)
+
+# Add the title
+plt.title('Fuel efficiency vs Horse-power')
+
+# Add the x-axis label
+plt.xlabel('Horse-power')
+
+# Add the y-axis label
+plt.ylabel('Fuel efficiency (mpg)')
+
+# Display the plot
+plt.show()
+
+
+
+### Box Plots
+
+# Make a list of the column names to be plotted: cols
+cols = ['weight', 'mpg']
+
+# Generate the box plots
+df[cols].plot(kind='box', subplots=True)
+
+# Display the plot
+plt.show()
+
+
+
+
+### Histograms
+
+# This formats the plots such that they appear on separate rows
+fig, axes = plt.subplots(nrows=2, ncols=1)
+
+# Plot the PDF
+df.fraction.plot(ax=axes[0], kind='hist', bins=30, normed=True, range=(0,.3))
+plt.show()
+
+# Plot the CDF
+df.fraction.plot(ax=axes[0], kind='hist', bins=30, cumulative=True, normed=True, range=(0,.3))
+plt.show()
