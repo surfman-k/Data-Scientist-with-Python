5f360f75149c74fafcaa0ea1497a8a55482df4d1
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-03 22:30:40 -0500

    Boolean Indexing

--- pandas Foundations/exploratory-data-analysis.py
+++ pandas Foundations/exploratory-data-analysis.py
@@ -123,3 +123,9 @@ print(january.mean(), march.mean())
 
 # Print the standard deviation of the January and March data
 print(january.std(), march.std())
+
+
+######################
+## Boolean Indexing ##
+######################
+
