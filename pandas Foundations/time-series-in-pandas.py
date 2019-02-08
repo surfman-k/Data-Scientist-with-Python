e70f2f2224c8f5cab9aac09b132edefc030262ed
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-07 19:13:07 -0500

    Resampling Data

--- pandas Foundations/time-series-in-pandas.py
+++ pandas Foundations/time-series-in-pandas.py
@@ -42,3 +42,22 @@ sum14 = ts1 + ts4
 ## Resampling time series ##
 ############################
 
+# Downsample to 6 hour data and aggregate by mean: df1
+df1 = df.Temperature.resample('6h').mean()
+
+# Downsample to daily data and count the number of data points: df2
+df2 = df.Temperature.resample('D').count()
+
+# Extract temperature data for August: august
+august = df.Temperature['2010-August']
+
+# Downsample to obtain only the daily highest temperatures in August: august_highs
+august_highs = august.resample('D').max()
+
+# Extract temperature data for February: february
+february = df.Temperature.loc['2010-February']
+
+# Downsample to obtain the daily lowest temperatures in February: february_lows
+february_lows = february.resample('D').min()
+
+
