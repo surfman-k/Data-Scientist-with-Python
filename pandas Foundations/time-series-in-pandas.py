577d467906a8d91bf5039234b6a9812863c4999c
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-08 21:40:41 -0500

    Resampling

--- pandas Foundations/time-series-in-pandas.py
+++ pandas Foundations/time-series-in-pandas.py
@@ -74,3 +74,14 @@ august = pd.DataFrame({'smoothed':smoothed, 'unsmoothed':unsmoothed})
 # Plot both smoothed and unsmoothed data using august.plot().
 august.plot()
 plt.show()
+
+
+# Extract the August 2010 data: august
+august = df['Temperature']['August-2010']
+
+# Resample to daily data, aggregating by max: daily_highs
+daily_highs = august.resample('D').max()
+
+# Use a rolling 7-day window with method chaining to smooth the daily high temperatures in August
+daily_highs_smoothed = daily_highs.rolling(window=7).mean()
+print(daily_highs_smoothed)
