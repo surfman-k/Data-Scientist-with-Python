2d61895aa34a44cd95ee8ff55b74369de504018a
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-08 21:53:12 -0500

    Missing values and interpolation

--- pandas Foundations/time-series-in-pandas.py
+++ pandas Foundations/time-series-in-pandas.py
@@ -101,3 +101,13 @@ daily_departures = dallas.resample('D').sum()
 
 # Generate the summary statistics for daily Dallas departures: stats
 stats = daily_departures.describe()
+
+
+# Reset the index of ts2 to ts1, and then use linear interpolation to fill in the NaNs: ts2_interp
+ts2_interp = ts2.reindex(ts1.index).interpolate(how='linear')
+
+# Compute the absolute difference of ts1 and ts2_interp: differences 
+differences = np.abs(ts2_interp - ts1)
+
+# Generate and print summary statistics of the differences
+print(differences.describe())
