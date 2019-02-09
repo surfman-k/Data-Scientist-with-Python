aba39cabff0e4e14a7226cd66dfe8914c9a1e59c
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-08 21:32:18 -0500

    Smoothing

--- pandas Foundations/time-series-in-pandas.py
+++ pandas Foundations/time-series-in-pandas.py
@@ -61,3 +61,16 @@ february = df.Temperature.loc['2010-February']
 february_lows = february.resample('D').min()
 
 
+
+# Extract data from 2010-Aug-01 to 2010-Aug-15: unsmoothed
+unsmoothed = df['Temperature']['2010-Aug-01':'2010-Aug-15']
+
+# Apply a rolling mean with a 24 hour window: smoothed
+smoothed = unsmoothed.rolling(window=24).mean()
+
+# Create a new DataFrame with columns smoothed and unsmoothed: august
+august = pd.DataFrame({'smoothed':smoothed, 'unsmoothed':unsmoothed})
+
+# Plot both smoothed and unsmoothed data using august.plot().
+august.plot()
+plt.show()
