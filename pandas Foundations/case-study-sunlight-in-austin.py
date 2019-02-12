4dd6d0fc34513c27ce25ba457353927d1137cb5d
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-11 22:28:44 -0500

    Comparing Observed weather data from two sources

--- pandas Foundations/case-study-sunlight-in-austin.py
+++ pandas Foundations/case-study-sunlight-in-austin.py
@@ -1,3 +1,24 @@
 #####################################
 ## Case Study - Sunlight in Austin ##
 #####################################
+
+# Comparing Observed weather data from two sources (both from the National Oceanic & Atmospheric Administration)
+# - Austin, TX from 1981 to 2010
+# - Austin, TX Weather Data from 2011
+
+# Import pandas
+import pandas as pd
+
+# Read in the data file: df
+df = pd.read_csv(data_file)
+
+# Print the output of df.head()
+print(df.head())
+
+# Read in the data file with header=None: df_headers
+df_headers = pd.read_csv(data_file, header=None)
+
+# Print the output of df_headers.head()
+print(df_headers.head())
+
+
