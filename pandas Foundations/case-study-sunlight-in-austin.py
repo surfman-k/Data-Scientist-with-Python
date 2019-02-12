1924deb45615eb8321b030bdf99504e0b9f2bd51
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-11 22:31:58 -0500

    Re-assigning column names

--- pandas Foundations/case-study-sunlight-in-austin.py
+++ pandas Foundations/case-study-sunlight-in-austin.py
@@ -22,3 +22,14 @@ df_headers = pd.read_csv(data_file, header=None)
 print(df_headers.head())
 
 
+# Split on the comma to create a list: column_labels_list
+column_labels_list = column_labels.split(',')
+
+# Assign the new column labels to the DataFrame: df.columns
+df.columns = column_labels_list
+
+# Remove the appropriate columns: df_dropped
+df_dropped = df.drop(list_to_drop, axis='columns')
+
+# Print the output of df_dropped.head()
+print(df_dropped.head())
