a0904bebb8ce5d102d0e06c2d4dc48643ca93719
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-13 22:59:20 -0500

    Signal min, max, median

--- pandas Foundations/case-study-sunlight-in-austin.py
+++ pandas Foundations/case-study-sunlight-in-austin.py
@@ -73,3 +73,12 @@ df_clean['dew_point_faren'] = pd.to_numeric(df_clean['dew_point_faren'], errors=
 
 
 ### Statistical Exploratory Data Analysis
+
+# Print the median of the dry_bulb_faren column
+print(df_clean['dry_bulb_faren'].median())
+
+# Print the median of the dry_bulb_faren column for the time range '2011-Apr':'2011-Jun'
+print(df_clean.loc['2011-Apr':'2011-Jun', 'dry_bulb_faren'].median())
+
+# Print the median of the dry_bulb_faren column for the month of January
+print(df_clean.loc['2011-Jan', 'dry_bulb_faren'].median())
