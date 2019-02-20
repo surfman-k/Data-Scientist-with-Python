9aec70ea07a70297960677c977d92170a91b2039
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-19 19:55:53 -0500

    weekly avg temp and visibility

--- pandas Foundations/case-study-sunlight-in-austin.py
+++ pandas Foundations/case-study-sunlight-in-austin.py
@@ -138,3 +138,19 @@ overcast_daily_max_mean = overcast_daily_max.mean()
 
 # Print the difference (sunny minus overcast)
 print(sunny_daily_max_mean - overcast_daily_max_mean)
+
+
+### Weekly average temperature and visibility
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Select the visibility and dry_bulb_faren columns and resample them: weekly_mean
+weekly_mean = df_clean[['visibility','dry_bulb_faren']].resample('W').mean()
+
+# Print the output of weekly_mean.corr()
+print(weekly_mean.corr())
+
+# Plot weekly_mean with subplots=True
+weekly_mean.plot(subplots=True)
+plt.show()
