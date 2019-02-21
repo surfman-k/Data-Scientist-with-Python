b5c227f0b7f0ea4d091b6489abe57f3bda62d9ff
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-20 21:16:20 -0500

    Images and humidity

--- pandas Foundations/case-study-sunlight-in-austin.py
+++ pandas Foundations/case-study-sunlight-in-austin.py
@@ -196,3 +196,15 @@ sunny_fraction = sunny_hours / total_hours
 # Make a box plot of sunny_fraction
 sunny_fraction.plot(kind='box')
 plt.show()
+
+
+### Humidity
+
+# Resample dew_point_faren and dry_bulb_faren by Month, aggregating the maximum values: monthly_max
+monthly_max = df_clean[['dew_point_faren', 'dry_bulb_faren']].resample('M').max()
+
+# Generate a histogram with bins=8, alpha=0.5, subplots=True
+monthly_max.plot(kind='hist', bins=8, alpha=0.5, subplots=True)
+
+# Show the plot
+plt.show()
