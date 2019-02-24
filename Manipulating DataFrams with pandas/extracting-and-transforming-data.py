48dff67ea58defe44eade8141cad6b97137050eb
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-23 19:13:22 -0500

    Slicing columns

--- Manipulating DataFrams with pandas/extracting-and-transforming-data.py
+++ Manipulating DataFrams with pandas/extracting-and-transforming-data.py
@@ -29,3 +29,24 @@ p_counties_rev = election.loc['Potter':'Perry':-1]
 
 # Print the p_counties_rev DataFrame
 print(p_counties_rev)
+
+
+### Slicing Columns
+
+# Slice the columns from the starting column to 'Obama': left_columns
+left_columns = election.loc[:,:'Obama']
+
+# Print the output of left_columns.head()
+print(left_columns.head())
+
+# Slice the columns from 'Obama' to 'winner': middle_columns
+middle_columns = election.loc[:,'Obama':'winner']
+
+# Print the output of middle_columns.head()
+print(middle_columns.head())
+
+# Slice the columns from 'Romney' to the end: 'right_columns'
+right_columns = election.loc[:,'Romney':]
+
+# Print the output of right_columns.head()
+print(right_columns.head())
