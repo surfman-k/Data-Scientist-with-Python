ef6c10552c73338b451e2716f4433c016a4aa187
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-24 17:34:30 -0500

    Filtering Columns using other columns

--- Manipulating DataFrams with pandas/extracting-and-transforming-data.py
+++ Manipulating DataFrams with pandas/extracting-and-transforming-data.py
@@ -79,3 +79,16 @@ high_turnout_df = election[high_turnout]
 
 # Print the high_turnout_results DataFrame
 print(high_turnout_df)
+
+
+# Import numpy
+import numpy as np
+
+# Create the boolean array: too_close
+too_close = election.margin < 1
+
+# Assign np.nan to the 'winner' column where the results were too close to call
+election.winner[too_close] = np.nan
+
+# Print the output of election.info()
+print(election.info())
