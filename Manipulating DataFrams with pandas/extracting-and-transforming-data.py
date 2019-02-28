75e20836947915541d4c9d5736d189d7649fe929
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-27 22:16:55 -0500

    Using Vectorized Functions

--- Manipulating DataFrams with pandas/extracting-and-transforming-data.py
+++ Manipulating DataFrams with pandas/extracting-and-transforming-data.py
@@ -140,3 +140,21 @@ election['color'] = election.winner.map(red_vs_blue)
 
 # Print the output of election.head()
 print(election.head())
+
+
+# Using Vectorized Functions
+
+# Import zscore from scipy.stats
+from scipy.stats import zscore
+
+# Call zscore with election['turnout'] as input: turnout_zscore
+turnout_zscore = zscore(election['turnout'])
+
+# Print the type of turnout_zscore
+print(type(turnout_zscore))
+
+# Assign turnout_zscore to a new column: election['turnout_zscore']
+election['turnout_zscore'] = turnout_zscore
+
+# Print the output of election.head()
+print(election.head())
