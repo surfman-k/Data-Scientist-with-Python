9aa2cabdcaf58327494292a5a954aa18fcee1db6
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-02-27 22:12:10 -0500

    transforming dataframes

--- Manipulating DataFrams with pandas/extracting-and-transforming-data.py
+++ Manipulating DataFrams with pandas/extracting-and-transforming-data.py
@@ -112,5 +112,17 @@ print(df.dropna(how='all').shape)
 print(titanic.dropna(thresh=1000, axis='columns').info())
 
 
-
 ### Transforming DataFrames
+
+# Write a function to convert degrees Fahrenheit to degrees Celsius: to_celsius
+def to_celsius(F):
+    return 5/9*(F - 32)
+
+# Apply the function over 'Mean TemperatureF' and 'Mean Dew PointF': df_celsius
+df_celsius = weather[['Mean TemperatureF','Mean Dew PointF']].apply(to_celsius)
+
+# Reassign the columns df_celsius
+df_celsius.columns = ['Mean TemperatureC', 'Mean Dew PointC']
+
+# Print the output of df_celsius.head()
+print(df_celsius.head())
