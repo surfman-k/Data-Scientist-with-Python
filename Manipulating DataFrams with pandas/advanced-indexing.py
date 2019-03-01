d61f0a2e4035809d351dbc3d3ded81634f0dbf96
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-01 18:12:34 -0500

    Changing index of a DataFrame

--- Manipulating DataFrams with pandas/advanced-indexing.py
+++ Manipulating DataFrams with pandas/advanced-indexing.py
@@ -1,3 +1,14 @@
 ####################################
 ## Index objects and labeled data ##
 ####################################
+
+# Indexes do not support mutable operations!
+
+# Create the list of new indexes: new_idx
+new_idx = [str.upper() for str in sales.index]
+
+# Assign new_idx to sales.index
+sales.index = new_idx
+
+# Print the sales DataFrame
+print(sales)
