829ee6fdd4a1b2b4ff68b90e9ac28713ff511fa2
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-02 18:25:40 -0500

    Building index first, then DataFrame

--- Manipulating DataFrams with pandas/advanced-indexing.py
+++ Manipulating DataFrams with pandas/advanced-indexing.py
@@ -25,3 +25,20 @@ sales.columns.name = 'PRODUCTS'
 
 # Print the sales dataframe again
 print(sales)
+
+
+# Building an Index, then a DataFrame
+
+# Generate the list of months: months
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+
+# Assign months to sales.index
+sales.index = months
+
+# Print the modified sales DataFrame
+print(sales)
+
+
+#######################
+## Advanced Indexing ##
+#######################
