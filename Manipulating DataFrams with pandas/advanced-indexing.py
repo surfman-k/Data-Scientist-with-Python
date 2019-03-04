8e7dbdc1e2be05806a4cd4a6581ee76d44d64760
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-04 18:13:48 -0500

    Accessing Data with Non-Unique Indexes

--- Manipulating DataFrams with pandas/advanced-indexing.py
+++ Manipulating DataFrams with pandas/advanced-indexing.py
@@ -75,3 +75,15 @@ sales = sales.sort_index()
 
 # Print the sales DataFrame
 print(sales)
+
+
+## Accessing Data with Non-Unique Indexes
+
+# Set the index to the column 'state': sales
+sales = sales.set_index(['state'])
+
+# Print the sales DataFrame
+print(sales)
+
+# Access the data from 'NY'
+print(sales.loc['NY'])
