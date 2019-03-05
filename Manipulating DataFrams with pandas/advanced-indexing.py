c17b0020469b3627dd3b73d44d6482499d3f8a7b
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-05 12:18:06 -0500

    Indexing multiple levels of a MultiIndex

--- Manipulating DataFrams with pandas/advanced-indexing.py
+++ Manipulating DataFrams with pandas/advanced-indexing.py
@@ -87,3 +87,15 @@ print(sales)
 
 # Access the data from 'NY'
 print(sales.loc['NY'])
+
+
+### Indexing multiple levels of a MultiIndex
+
+# Look up data for NY in month 1: NY_month1
+NY_month1 = sales.loc[('NY', 1)]
+
+# Look up data for CA and TX in month 2: CA_TX_month2
+CA_TX_month2 = sales.loc[(['CA', 'TX'], 2), :]
+
+# Look up data for all states in month 2: all_month2
+all_month2 = sales.loc[(slice(None), 2), :]
