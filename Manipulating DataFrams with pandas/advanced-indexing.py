f69622ddc7be496116abbe61717920a2152884ec
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-08 19:30:46 -0500

    stacking dataframes

--- Manipulating DataFrams with pandas/advanced-indexing.py
+++ Manipulating DataFrams with pandas/advanced-indexing.py
@@ -122,3 +122,17 @@ pivot = users.pivot(index='weekday', columns='city')
 
 # Print the pivoted DataFrame
 print(pivot)
+
+
+#########################
+## Stacking DataFrames ##
+#########################
+
+# Unstack users by 'weekday': byweekday
+byweekday = users.unstack(level='weekday')
+
+# Print the byweekday DataFrame
+print(byweekday)
+
+# Stack byweekday by 'weekday' and print it
+print(byweekday.stack(level='weekday'))
