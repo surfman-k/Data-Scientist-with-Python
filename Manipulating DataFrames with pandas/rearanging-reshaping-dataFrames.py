0256a4c5d0a6b1b81a6a58ea19e7291786b1c6d1
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-14 16:53:47 -0400

    obtaining key-value pairs with melt()

--- Manipulating DataFrames with pandas/rearanging-reshaping-dataFrames.py
+++ Manipulating DataFrames with pandas/rearanging-reshaping-dataFrames.py
@@ -91,3 +91,17 @@ skinny = pd.melt(users, id_vars=['weekday', 'city'])
 
 # Print skinny
 print(skinny)
+
+
+
+# Set the new index: users_idx
+users_idx = users.set_index(['city', 'weekday'])
+
+# Print the users_idx DataFrame
+print(users_idx)
+
+# Obtain the key-value pairs: kv_pairs
+kv_pairs = pd.melt(users_idx, col_level=0)
+
+# Print the key-value pairs
+print(kv_pairs)
