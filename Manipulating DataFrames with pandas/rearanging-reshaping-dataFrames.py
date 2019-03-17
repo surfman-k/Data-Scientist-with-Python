c780b4608b3ac5e0fd46e726d2c065288a8a9e71
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-16 21:42:11 -0400

    pivot table

--- Manipulating DataFrames with pandas/rearanging-reshaping-dataFrames.py
+++ Manipulating DataFrames with pandas/rearanging-reshaping-dataFrames.py
@@ -110,3 +110,10 @@ print(kv_pairs)
 ##################
 ## Pivot Tables ##
 ##################
+
+# Create the DataFrame with the appropriate pivot table: by_city_day
+by_city_day = users.pivot_table(index='weekday', columns='city')
+
+# Print by_city_day
+print(by_city_day)
+
