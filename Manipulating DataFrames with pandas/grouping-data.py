37ebadd79d4b314cfc7c92e1d1d80a2ae29d2ed4
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-18 19:45:46 -0400

    Categoricals and groupby

---
+++ Manipulating DataFrames with pandas/grouping-data.py
@@ -0,0 +1,21 @@
+#############################
+## Categoricals and roupby ##
+#############################
+
+# Group titanic by 'pclass'
+by_class = titanic.groupby('pclass')
+
+# Aggregate 'survived' column of by_class by count
+count_by_class = titanic.groupby('pclass')['survived'].count()
+
+# Print count_by_class
+print(count_by_class)
+
+# Group titanic by 'embarked' and 'pclass'
+by_mult = titanic.groupby(['embarked','pclass'])
+
+# Aggregate 'survived' column of by_mult by count
+count_mult = by_mult['survived'].count()
+
+# Print count_mult
+print(count_mult)
