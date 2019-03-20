b3a10220683c2c978a101cab9ab988cfcf681c67
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-20 18:28:56 -0400

    Case Study : Summer Olympics

---
+++ Manipulating DataFrames with pandas/case-study-summer-olympics.py
@@ -0,0 +1,7 @@
+##################################
+## Case Study : Summer Olympics ##
+##################################
+
+# Olympic Games Data from The Guardian from 1896 to 2012
+# Exploring different patterns in the data
+
