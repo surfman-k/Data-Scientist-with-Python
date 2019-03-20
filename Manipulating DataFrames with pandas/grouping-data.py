d0d9862dbf5504d943f537c7c8e55616ef47f937
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-20 17:50:19 -0400

    groupby and filtering

--- Manipulating DataFrames with pandas/grouping-data.py
+++ Manipulating DataFrames with pandas/grouping-data.py
@@ -130,3 +130,51 @@ reg_disp = regional.apply(disparity)
 
 # Print the disparity of 'United States', 'United Kingdom', and 'China'
 print(reg_disp.loc[['United States','United Kingdom','China']])
+
+
+
+###########################
+## groupby and filtering ##
+###########################
+
+# Create a groupby object using titanic over the 'sex' column: by_sex
+by_sex = titanic.groupby('sex')
+
+# Call by_sex.apply with the function c_deck_survival
+c_surv_by_sex = by_sex.apply(c_deck_survival)
+
+# Print the survival rates
+print(c_surv_by_sex)
+
+
+
+### Using filter()
+
+# Read the CSV file into a DataFrame: sales
+sales = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+# Group sales by 'Company': by_company
+by_company = sales.groupby('Company')
+
+# Compute the sum of the 'Units' of by_company: by_com_sum
+by_com_sum = by_company['Units'].sum()
+print(by_com_sum)
+
+# Filter 'Units' where the sum is > 35: by_com_filt
+by_com_filt = by_company.filter(lambda g:g['Units'].sum() > 35)
+print(by_com_filt)
+
+
+
+###  Using map()
+
+# Create the Boolean Series: under10
+under10 = (titanic['age'] < 10).map({True:'under 10', False:'over 10'})
+
+# Group by under10 and compute the survival rate
+survived_mean_1 = titanic.groupby(under10)['survived'].mean()
+print(survived_mean_1)
+
+# Group by under10 and pclass and compute the survival rate
+survived_mean_2 = titanic.groupby([under10, 'pclass'])['survived'].mean()
+print(survived_mean_2)
