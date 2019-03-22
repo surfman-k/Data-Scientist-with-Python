9a2edff2ef6c851d81d608c4e2457d20a2a0cd71
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-21 21:28:22 -0400

    Counting medals

--- Manipulating DataFrames with pandas/case-study-summer-olympics.py
+++ Manipulating DataFrames with pandas/case-study-summer-olympics.py
@@ -5,3 +5,31 @@
 # Olympic Games Data from The Guardian from 1896 to 2012
 # Exploring different patterns in the data
 
+
+
+### Medals by Country
+
+# Select the 'NOC' column of medals: country_names
+country_names = medals['NOC']
+
+# Count the number of medals won by each country: medal_counts
+medal_counts = country_names.value_counts()
+
+# Print top 15 countries ranked by medals
+print(medal_counts.head(15))
+
+
+
+### Counting Medals by Type using Pivot Table
+
+# Construct the pivot table: counted
+counted = medals.pivot_table(index='NOC', values='Athlete', columns='Medal', aggfunc='count')
+
+# Create the new column: counted['totals']
+counted['totals'] = counted.sum(axis='columns')
+
+# Sort counted by the 'totals' column
+counted = counted.sort_values('totals', ascending=False)
+
+# Print the top 15 rows of counted
+print(counted.head(15))
