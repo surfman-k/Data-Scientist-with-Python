010d1d56ee55ef548753364db617ab6d2584c49c
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-24 21:52:25 -0400

    Delete introduction-to-DataCamp-projects.py

--- Projects/introduction-to-DataCamp-projects.py
+++
@@ -1,5 +0,0 @@
-###############################
-## First Project on DataCamp ##
-###############################
-
-A Jupyter notebook is a document that contains text cells (what you're reading right now) and code cells. What is special with a notebook is that it's interactive: You can change or add code cells, and then run a cell by first selecting it and then clicking the run cell button above ( ▶| Run ) or hitting ctrl + enter.
