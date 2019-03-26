f5008ce9b9a16571ed2ba3dae7fb442e188822a5
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-26 17:43:34 -0400

    Combining Multiple Worksheets

--- Importing & Managing Financial Data in Python/importing-stock-listing-data-from-excel.py
+++ Importing & Managing Financial Data in Python/importing-stock-listing-data-from-excel.py
@@ -50,3 +50,45 @@ listings = pd.read_excel(xls, sheet_name=exchanges, na_values='n/a')
 
 # Inspect NASDAQ listings
 listings['nasdaq'].info()
+
+
+###################################
+## Combining Multiple Worksheets ## 
+###################################
+
+# Import the NYSE and NASDAQ listings
+nyse = pd.read_excel('listings.xlsx', sheet_name='nyse', na_values='n/a')
+nasdaq = pd.read_excel('listings.xlsx', sheet_name='nasdaq', na_values='n/a')
+
+# Inspect nyse and nasdaq
+nyse.info()
+nasdaq.info()
+
+# Add Exchange reference columns
+nyse['Exchange'] = 'NYSE'
+nasdaq['Exchange'] = 'NASDAQ'
+
+# Concatenate DataFrames  
+combined_listings = pd.concat([nyse, nasdaq]) 
+
+
+# Create the pd.ExcelFile() object
+xls = pd.ExcelFile('listings.xlsx')
+
+# Extract the sheet names from xls
+exchanges = xls.sheet_names
+
+# Create an empty list: listings
+listings = []
+
+# Import the data
+for exchange in exchanges:
+    listing = pd.read_excel(xls, sheetname=exchange, na_values='n/a')
+    listing['Exchange'] = exchange
+    listings.append(listing)
+
+# Concatenate the listings: listing_data
+listing_data = pd.concat(listings)
+
+# Inspect the results
+listing_data.info()
