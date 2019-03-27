aca48f58312eb90fcb8d8d88b39c85233a22fff5
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-27 17:29:46 -0400

    Financial data from the Federal Reserve

--- Importing & Managing Financial Data in Python/importing-financial-data-from-the-web.py
+++ Importing & Managing Financial Data in Python/importing-financial-data-from-the-web.py
@@ -52,3 +52,63 @@ plt.show()
 ## Financial data from the Federal Reserve ## 
 #############################################
 
+### Visualize the long-term gold price trend
+
+# Set start date
+start = date(1968, 1, 1)
+
+# Set series code
+series = 'GOLDAMGBD228NLBM'
+
+# Import the data
+gold_price = DataReader(series, 'fred', start=start)
+
+# Inspect the price of gold
+gold_price.info()
+
+# Plot the price of gold
+gold_price.plot()
+
+# Show the plot
+plt.show()
+
+
+### Compare labor market participation and unemployment rates
+
+# Set the start date
+start = date(1950,1,1)
+
+# Define the series codes
+series = ['UNRATE', 'CIVPART']
+
+# Import the data
+econ_data = DataReader(series, 'fred', start)
+
+# Assign new column labels
+econ_data.columns = ['Unemployment Rate', 'Participation Rate']
+
+# Plot econ_data
+econ_data.plot(title='Labor Market', subplots=True)
+
+# Show the plot
+plt.show()
+
+
+### Compare bond and stock performance
+
+# Set the start date
+start = date(2008,1,1)
+
+# Set the series codes
+series = ['BAMLHYH0A0HYM2TRIV', 'SP500']
+
+# Import the data
+data = DataReader(series, 'fred', start)
+
+# Plot the results
+data.plot(title='Performance Comparison', subplots=True)
+
+# Show the plot
+plt.show()
+
+
