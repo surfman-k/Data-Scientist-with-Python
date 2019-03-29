5974d8a8c421080ba8af65184acf43752cb65fbe
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-29 16:53:09 -0400

    List the poorest and richest countries worldwide

---
+++ Importing & Managing Financial Data in Python/summarizing-data-and-visualizing-results.py
@@ -0,0 +1,28 @@
+######################################################
+## List the poorest and richest countries worldwide ## 
+######################################################
+
+# Import the data
+income = pd.read_csv('per_capita_income.csv')
+
+# Inspect the result
+income.info()
+
+# Sort the data by income
+income = income.sort_values('Income per Capita', ascending=False)
+
+# Display the first and last five rows
+print(income.head())
+print(income.tail())
+
+# Calculate the mean
+print(income['Income per Capita'].mean())
+
+# Calculate the median
+print(income['Income per Capita'].median())
+
+# Create the new column
+income['Income per Capita (,000)'] = income['Income per Capita'] // 1000
+
+# Calculate the mode of the new column
+income['Income per Capita (,000)'].mode()
