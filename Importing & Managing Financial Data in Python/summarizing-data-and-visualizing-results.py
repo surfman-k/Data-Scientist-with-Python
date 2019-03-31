2bf949001ecac93f0479ef68c825fe07c5e68fb5
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-03-30 22:46:26 -0400

    Quantiles: Dispersion

--- Importing & Managing Financial Data in Python/summarizing-data-and-visualizing-results.py
+++ Importing & Managing Financial Data in Python/summarizing-data-and-visualizing-results.py
@@ -26,3 +26,26 @@ income['Income per Capita (,000)'] = income['Income per Capita'] // 1000
 
 # Calculate the mode of the new column
 income['Income per Capita (,000)'].mode()
+
+
+###########################
+## Quantiles: Dispersion ## 
+###########################
+
+# Calculate mean
+mean = income['Income per Capita'].mean()
+
+# Calculate standard deviation 
+std = income['Income per Capita'].std()
+
+# Calculate and print lower and upper bounds
+bounds = [mean - std, mean + std]
+print(bounds)
+
+# Calculate and print first and third quartiles
+quantiles = income['Income per Capita'].quantile([.25, .75])
+print(quantiles)
+
+# Calculate and print IQR
+iqr = quantiles[.75] - quantiles[.25]
+print(iqr)
