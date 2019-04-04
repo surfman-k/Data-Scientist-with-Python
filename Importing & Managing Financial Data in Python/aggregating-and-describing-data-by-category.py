ec32d7cc1ad1b70365f55ba7c21a6b165811afcb
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-04 17:50:03 -0400

    All summary statistics by sector

--- Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
+++ Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
@@ -54,3 +54,30 @@ ipo_by_year.plot(kind='bar')
 
 # Show the plot
 plt.show()
+
+
+### All summary statistics by sector
+
+# Inspect NASDAQ data
+nasdaq.info()
+
+# Create market_cap_m
+nasdaq['market_cap_m'] = nasdaq['Market Capitalization'].div(1e6)
+
+# Drop the Market Capitalization column
+nasdaq.drop('Market Capitalization', axis=1, inplace=True)
+
+# Group nasdaq by Sector
+nasdaq_by_sector = nasdaq.groupby('Sector')
+
+# Create summary statistics by sector
+summary = nasdaq_by_sector.describe()
+
+# Print the summary
+print(summary)
+
+# Unstack 
+summary = summary.unstack()
+
+# Print the summary again
+print(summary)
