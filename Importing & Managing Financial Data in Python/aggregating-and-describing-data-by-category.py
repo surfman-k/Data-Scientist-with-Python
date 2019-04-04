3e7ebb8168e51fca795e0e4ee18babc6410d00a0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-04 17:42:28 -0400

    Median market capitalization by sector

--- Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
+++ Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
@@ -1,3 +1,29 @@
 ######################################################
 ## Aggregating and describing your data by category ## 
 ######################################################
+
+### Median market capitalization by sector
+
+# Inspect NYSE data
+nyse.info()
+
+# Create market_cap_m
+nyse['market_cap_m'] = nyse['Market Capitalization'].div(1e6)
+
+# Drop market cap column
+nyse = nyse.drop('Market Capitalization', axis=1)
+
+# Group nyse by sector
+mcap_by_sector = nyse.groupby('Sector')
+
+# Calculate median
+median_mcap_by_sector = mcap_by_sector.market_cap_m.median()
+
+# Plot and show as horizontal bar chart
+median_mcap_by_sector.plot(kind='barh', title='NYSE - Median Market Capitalization')
+
+# Add the label
+plt.xlabel('USD mn')
+
+# Show the plot
+plt.show()
