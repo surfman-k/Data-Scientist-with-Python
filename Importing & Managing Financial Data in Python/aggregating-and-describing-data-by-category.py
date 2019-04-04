aad9773f64f7c98b0b1783f8f863d5244ca426a4
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-04 17:47:51 -0400

    Median market capitalization by IPO year

--- Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
+++ Importing & Managing Financial Data in Python/aggregating-and-describing-data-by-category.py
@@ -27,3 +27,30 @@ plt.xlabel('USD mn')
 
 # Show the plot
 plt.show()
+
+
+### Median market capitalization by IPO year
+
+# Inspect listings
+listings.info()
+
+# Show listings head
+print(listings.head())
+
+# Create market_cap_m
+listings['market_cap_m'] = listings['Market Capitalization'].div(1e6)
+
+# Select companies with IPO after 1985
+listings = listings[listings['IPO Year'] > 1985]
+
+# Drop missing values and convert to integers
+listings['IPO Year'] = listings['IPO Year'].dropna().astype(int)
+
+# Calculate the median market cap by IPO Year and sort the index
+ipo_by_year = listings.groupby('IPO Year').market_cap_m.median().sort_index()
+
+# Plot results as a bar chart
+ipo_by_year.plot(kind='bar')
+
+# Show the plot
+plt.show()
