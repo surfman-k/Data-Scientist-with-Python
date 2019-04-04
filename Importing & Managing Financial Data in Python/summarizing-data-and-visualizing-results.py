825ee013b7050029298aaf700f3f698abf61fc97
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-03 22:37:21 -0400

    Technology IPOs by year on all exchanges

--- Importing & Managing Financial Data in Python/summarizing-data-and-visualizing-results.py
+++ Importing & Managing Financial Data in Python/summarizing-data-and-visualizing-results.py
@@ -151,3 +151,36 @@ for exchange in exchanges:
     sectors.sort_values(ascending=False).plot(kind='bar')
     # Show the plot
     plt.show()
+
+
+
+### Technology IPOs by year on all exchanges
+
+# Create lists
+exchanges = ['amex', 'nasdaq', 'nyse']
+all_listings = []
+
+# Use for loop to create listing_data
+for exchange in exchanges:
+    all_listings.append(listings[exchange])
+    
+# Combine DataFrames
+listing_data = pd.concat(all_listings)
+
+# Select tech companies
+tech_companies = listing_data[listing_data.Sector == 'Technology']
+
+# Create ipo_years
+ipo_years = tech_companies['IPO Year']
+
+# Drop missing values and convert to int
+ipo_years = ipo_years.dropna().astype(int)
+
+# Count values, sort ascending by year, and create a bar plot
+ipo_years.value_counts(ascending=True).plot(kind='bar', title='Tech IPOs by Year')
+
+# Rotate xticks and show result
+plt.xticks(rotation=45)
+
+# Show the plot
+plt.show()
