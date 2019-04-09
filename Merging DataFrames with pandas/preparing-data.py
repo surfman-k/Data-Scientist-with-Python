e6c0ebba36b77c2c5436cea0a587716a749f4ff4
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-09 17:41:06 -0400

    Reading DataFrames from multiple files

--- Merging DataFrames with pandas/preparing-data.py
+++ Merging DataFrames with pandas/preparing-data.py
@@ -1,3 +1,57 @@
 ############################################
 ## Reading DataFrames from multiple files ## 
 ############################################
+
+# Import pandas
+import pandas as pd
+
+# Read 'Bronze.csv' into a DataFrame: bronze
+bronze = pd.read_csv('Bronze.csv')
+
+# Read 'Silver.csv' into a DataFrame: silver
+silver = pd.read_csv('Silver.csv')
+
+# Read 'Gold.csv' into a DataFrame: gold
+gold = pd.read_csv('Gold.csv')
+
+# Print the first five rows of gold
+print(gold.head())
+
+
+### Using a for loop
+
+# Import pandas
+import pandas as pd
+
+# Create the list of file names: filenames
+filenames = ['Gold.csv', 'Silver.csv', 'Bronze.csv']
+
+# Create the list of three DataFrames: dataframes
+dataframes = []
+for filename in filenames:
+    dataframes.append(pd.read_csv(filename))
+
+# Print top 5 rows of 1st DataFrame in dataframes
+print(dataframes[0].head())
+
+
+### Combining DataFrames
+
+# Import pandas
+import pandas as pd
+
+# Make a copy of gold: medals
+medals = gold.copy()
+
+# Create list of new column labels: new_labels
+new_labels = ['NOC', 'Country', 'Gold']
+
+# Rename the columns of medals using new_labels
+medals.columns = new_labels
+
+# Add columns 'Silver' & 'Bronze' to medals
+medals['Silver'] = silver['Total']
+medals['Bronze'] = bronze['Total']
+
+# Print the head of medals
+print(medals.head())
