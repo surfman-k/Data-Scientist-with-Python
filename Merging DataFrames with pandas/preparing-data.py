85fb5873b142523c5d44ad7c5c4361e5ad41e962
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-11 18:22:54 -0400

    Reindexing DataFrame from list

--- Merging DataFrames with pandas/preparing-data.py
+++ Merging DataFrames with pandas/preparing-data.py
@@ -88,3 +88,21 @@ weather4 = weather1.sort_values('Max TemperatureF')
 
 # Print the head of weather4
 print(weather4.head())
+
+
+### Reindexing DataFrame from list
+
+# Import pandas
+import pandas as pd
+
+# Reindex weather1 using the list year: weather2
+weather2 = weather1.reindex(year)
+
+# Print weather2
+print(weather2)
+
+# Reindex weather1 using the list year with forward-fill: weather3
+weather3 = weather1.reindex(year).ffill()
+
+# Print weather3
+print(weather3)
