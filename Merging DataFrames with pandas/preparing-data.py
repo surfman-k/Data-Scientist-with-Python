f5368ff01bf4b0bfa61101bef253cc32d73e9440
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-13 22:23:56 -0400

    Reindexing DataFrame from another DataFrame index

--- Merging DataFrames with pandas/preparing-data.py
+++ Merging DataFrames with pandas/preparing-data.py
@@ -106,3 +106,21 @@ weather3 = weather1.reindex(year).ffill()
 
 # Print weather3
 print(weather3)
+
+
+### Reindexing DataFrame from another DataFrame index
+
+# Import pandas
+import pandas as pd
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
