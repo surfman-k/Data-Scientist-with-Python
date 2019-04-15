f0c9cb1135feee7579de1567f10033660c0435d0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-14 23:48:44 -0400

    Arithmetics

--- Merging DataFrames with pandas/preparing-data.py
+++ Merging DataFrames with pandas/preparing-data.py
@@ -125,3 +125,69 @@ common_names = common_names.dropna()
 
 # Print shape of new common_names
 print(common_names.shape)
+
+
+
+### Broadcasting in arithmetic formulas
+
+# Extract selected columns from weather as new DataFrame: temps_f
+temps_f = weather[['Min TemperatureF','Mean TemperatureF','Max TemperatureF']]
+
+# Convert temps_f to celsius: temps_c
+temps_c = (temps_f - 32) * 5/9
+
+# Rename 'F' in column names with 'C': temps_c.columns
+temps_c.columns = temps_c.columns.str.replace('F', 'C')
+
+# Print first 5 rows of temps_c
+print(temps_c.head())
+
+
+### Computing percentage growth of GDP
+
+import pandas as pd
+
+# Read 'GDP.csv' into a DataFrame: gdp
+gdp = pd.read_csv('GDP.csv', index_col='DATE', parse_dates=True)
+
+# Slice all the gdp data from 2008 onward: post2008
+post2008 = gdp['2008':]
+
+# Print the last 8 rows of post2008
+print(post2008.tail(8))
+
+# Resample post2008 by year, keeping last(): yearly
+yearly = post2008.resample('A').last()
+
+# Print yearly
+print(yearly)
+
+# Compute percentage growth of yearly: yearly['growth']
+yearly['growth'] = yearly.pct_change() * 100
+
+# Print yearly again
+print(yearly)
+
+
+### Converting currency of stocks
+
+# Import pandas
+import pandas as pd
+
+# Read 'sp500.csv' into a DataFrame: sp500
+sp500 = pd.read_csv('sp500.csv',index_col='Date', parse_dates=True)
+
+# Read 'exchange.csv' into a DataFrame: exchange
+exchange = pd.read_csv('exchange.csv',index_col='Date', parse_dates=True)
+
+# Subset 'Open' & 'Close' columns from sp500: dollars
+dollars = sp500[['Open','Close']]
+
+# Print the head of dollars
+print(dollars.head())
+
+# Convert dollars to pounds: pounds
+pounds = dollars.multiply(exchange['GBP/USD'], axis='rows')
+
+# Print the head of pounds
+print(pounds.head())
