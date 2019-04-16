cb5f55af7773cc070985d1d7725ad671a8cc5644
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-15 23:37:06 -0400

    Concatenating pandas Series

--- Merging DataFrames with pandas/concatenating-data.py
+++ Merging DataFrames with pandas/concatenating-data.py
@@ -36,3 +36,20 @@ print(quarter1.loc['feb 26, 2015':'mar 7, 2015'])
 
 # Compute & print total sales in quarter1
 print(quarter1.sum())
+
+
+### Concatenating pandas Series
+
+# Initialize empty list: units
+units = []
+
+# Build the list of Series
+for month in [jan, feb, mar]:
+    units.append(month['Units'])
+
+# Concatenate the list: quarter1
+quarter1 = pd.concat(units, axis='rows')
+
+# Print slices from quarter1
+print(quarter1.loc['jan 27, 2015':'feb 2, 2015'])
+print(quarter1.loc['feb 26, 2015':'mar 7, 2015'])
