079f5c78c3ba7a68443cacc0760083628f72ec85
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-16 22:03:45 -0400

    Appending DataFrames with ignore_index

--- Merging DataFrames with pandas/concatenating-data.py
+++ Merging DataFrames with pandas/concatenating-data.py
@@ -58,3 +58,21 @@ print(quarter1.loc['feb 26, 2015':'mar 7, 2015'])
 ##########################################
 ## Appending & concatenating DataFrames ## 
 ##########################################
+
+### Appending DataFrames with ignore_index
+
+# Add 'year' column to names_1881 & names_1981
+names_1881['year'] = 1881
+names_1981['year'] = 1981
+
+# Append names_1981 after names_1881 with ignore_index=True: combined_names
+combined_names = names_1881.append(names_1981, ignore_index=True)
+
+# Print shapes of names_1981, names_1881, and combined_names
+print(names_1981.shape)
+print(names_1881.shape)
+print(combined_names.shape)
+
+# Print all rows that contain the name 'Morgan'
+print(combined_names.loc[combined_names['name']=='Morgan'])
+
