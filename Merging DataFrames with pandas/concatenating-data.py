f70f8706b9a80aa49b9b37340fb6926ce1234953
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-17 17:37:57 -0400

    Reading multiple files to build a DataFrame

--- Merging DataFrames with pandas/concatenating-data.py
+++ Merging DataFrames with pandas/concatenating-data.py
@@ -84,3 +84,28 @@ weather = pd.concat([weather_max, weather_mean], axis=1)
 
 # Print weather
 print(weather)
+
+
+### Reading multiple files to build a DataFrame
+
+for medal in medal_types:
+
+    # Create the file name: file_name
+    file_name = "%s_top5.csv" % medal
+    
+    # Create list of column names: columns
+    columns = ['Country', medal]
+    
+    # Read file_name into a DataFrame: df
+    medal_df = pd.read_csv(file_name, header=0, index_col='Country', names=columns)
+
+    # Append medal_df to medals
+    medals.append(medal_df)
+
+# Concatenate medals horizontally: medals
+medals = pd.concat(medals, axis='columns')
+
+# Print medals
+print(medals)
+
+
