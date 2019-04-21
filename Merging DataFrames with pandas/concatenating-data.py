f0dd2f47db9140c7bdfcb261e3bf5955892a7ef0
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-20 23:16:24 -0400

    Resampling & concatenating DataFrames with inner join

--- Merging DataFrames with pandas/concatenating-data.py
+++ Merging DataFrames with pandas/concatenating-data.py
@@ -224,3 +224,18 @@ medals = pd.concat(medal_list, keys=['bronze', 'silver', 'gold'], axis=1, join='
 
 # Print medals
 print(medals)
+
+
+### Resampling & concatenating DataFrames with inner join
+
+# Resample and tidy china: china_annual
+china_annual = china.resample('A').last().pct_change(10).dropna()
+
+# Resample and tidy us: us_annual
+us_annual = us.resample('A').last().pct_change(10).dropna()
+
+# Concatenate china_annual and us_annual: gdp
+gdp = pd.concat([china_annual, us_annual], axis=1, join='inner')
+
+# Resample gdp and print
+print(gdp.resample('10A').last())
