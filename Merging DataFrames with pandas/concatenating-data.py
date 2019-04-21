67de4cfb660b1ce7fb0f4bfde421ff79db8dcc70
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-20 23:14:35 -0400

    Concatenating DataFrames from a dict

--- Merging DataFrames with pandas/concatenating-data.py
+++ Merging DataFrames with pandas/concatenating-data.py
@@ -185,3 +185,27 @@ slice_2_8 = february.loc['2015-2-2':'2015-2-8', idx[:,'Company']]
 
 # Print slice_2_8
 print(slice_2_8)
+
+
+### Concatenating DataFrames from a dict
+
+# Make the list of tuples: month_list
+month_list = [('january', jan), ('february', feb), ('march', mar)]
+
+# Create an empty dictionary: month_dict
+month_dict = {}
+
+for month_name, month_data in month_list:
+
+    # Group month_data: month_dict[month_name]
+    month_dict[month_name] = month_data.groupby('Company').sum()
+
+# Concatenate data in month_dict: sales
+sales = pd.concat(month_dict)
+
+# Print sales
+print(sales)
+
+# Print all sales by Mediacore
+idx = pd.IndexSlice
+print(sales.loc[idx[:, 'Mediacore'], :])
