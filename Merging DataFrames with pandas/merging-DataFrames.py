71a6043a4710357c67565a1dd25d9085eec03b83
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-24 20:10:47 -0400

    Left & right merging on multiple columns

--- Merging DataFrames with pandas/merging-DataFrames.py
+++ Merging DataFrames with pandas/merging-DataFrames.py
@@ -39,3 +39,22 @@ combined = pd.merge(revenue, managers, on=['branch_id', 'city', 'state'])
 
 # Print combined
 print(combined)
+
+
+########################
+## Joining DataFrames ## 
+########################
+
+### Left & right merging on multiple columns
+
+# Merge revenue and sales: revenue_and_sales
+revenue_and_sales = pd.merge(revenue, sales, on=['city','state'], how='right')
+
+# Print revenue_and_sales
+print(revenue_and_sales)
+
+# Merge sales and managers: sales_and_managers
+sales_and_managers = pd.merge(sales, managers, left_on=['city','state'], right_on=['branch','state'], how='left')
+
+# Print sales_and_managers
+print(sales_and_managers)
