6dce93afa89f52e286de076bd1889f23141e8e4a
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-24 20:11:41 -0400

    Merging DataFrames with outer join

--- Merging DataFrames with pandas/merging-DataFrames.py
+++ Merging DataFrames with pandas/merging-DataFrames.py
@@ -58,3 +58,24 @@ sales_and_managers = pd.merge(sales, managers, left_on=['city','state'], right_o
 
 # Print sales_and_managers
 print(sales_and_managers)
+
+
+### Merging DataFrames with outer join
+
+# Perform the first merge: merge_default
+merge_default = pd.merge(sales_and_managers, revenue_and_sales)
+
+# Print merge_default
+print(merge_default)
+
+# Perform the second merge: merge_outer
+merge_outer = pd.merge(sales_and_managers, revenue_and_sales, how='outer')
+
+# Print merge_outer
+print(merge_outer)
+
+# Perform the third merge: merge_outer_on
+merge_outer_on = pd.merge(sales_and_managers, revenue_and_sales, on=['city','state'], how='outer')
+
+# Print merge_outer_on
+print(merge_outer_on)
