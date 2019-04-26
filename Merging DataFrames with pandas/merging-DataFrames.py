2c49dac1517010955af34d0d328c1eb4e8cc8ab2
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-25 22:43:58 -0400

    Using merge_asof()

--- Merging DataFrames with pandas/merging-DataFrames.py
+++ Merging DataFrames with pandas/merging-DataFrames.py
@@ -104,3 +104,22 @@ tx_weather_ffill = pd.merge_ordered(austin, houston, on='date', fill_method='ffi
 
 # Print tx_weather_ffill
 print(tx_weather_ffill)
+
+
+
+### Using merge_asof()
+
+# Merge auto and oil: merged
+merged = pd.merge_asof(auto, oil, left_on='yr', right_on='Date')
+
+# Print the tail of merged
+print(merged.tail())
+
+# Resample merged: yearly
+yearly = merged.resample('A',on='Date')[['mpg','Price']].mean()
+
+# Print yearly
+print(yearly)
+
+# Print yearly.corr()
+print(yearly.corr())
