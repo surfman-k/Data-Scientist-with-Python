85cd8d44382f08f2fe39659e7b0b743d8477a2ce
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-27 21:16:19 -0400

    Loading Olympic edition DataFrame

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -2,3 +2,20 @@
 ## Case Study: Summer Olympics ## 
 #################################
 
+### Loading Olympic edition DataFrame
+
+# Import pandas
+import pandas as pd
+
+# Create file path: file_path
+file_path = 'Summer Olympic medallists 1896 to 2008 - EDITIONS.tsv'
+
+# Load DataFrame from file_path: editions
+editions = pd.read_csv(file_path, sep='\t')
+
+# Extract the relevant columns: editions
+editions = editions[['Edition', 'Grand Total', 'City', 'Country']]
+
+# Print editions DataFrame
+print(editions)
+
