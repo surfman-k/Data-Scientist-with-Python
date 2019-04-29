65f304ad6e6c8f89f8c2a5aca8bf2f2d1d8e6e3c
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-28 23:31:34 -0400

    Update case-study-summer-olympics.py

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -33,6 +33,3 @@ ioc_codes = pd.read_csv(file_path)
 # Extract the relevant columns: ioc_codes
 ioc_codes = ioc_codes[['Country', 'NOC']]
 
-# Print first and last 5 rows of ioc_codes
-print(ioc_codes.head())
-print(ioc_codes.tail())
