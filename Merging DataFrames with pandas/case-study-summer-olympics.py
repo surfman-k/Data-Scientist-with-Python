b153104082b4f30c6edb305a026bcdf7c0bd6d16
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-04-30 23:11:07 -0400

    Counting medals by country/edition in a pivot table

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -66,3 +66,15 @@ medals = pd.concat(medals_dict, ignore_index=True)
 # Print first and last 5 rows of medals
 print(medals.head())
 print(medals.tail())
+
+
+#############################
+## Quantifying Performance ## 
+#############################
+
+# Construct the pivot_table: medal_counts
+medal_counts = medals.pivot_table(index='Edition', values='Athlete', columns='NOC', aggfunc='count')
+
+# Print the first & last 5 rows of medal_counts
+print(medal_counts.head())
+print(medal_counts.tail())
