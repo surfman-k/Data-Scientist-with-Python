237f4df51a60a6a084bdb0c38bef142d21019cfc
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-05-03 23:13:07 -0400

    Computing percentage change in fraction of medals won

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -110,3 +110,19 @@ fractions = medal_counts.divide(totals, axis='rows')
 # Print first & last 5 rows of fractions
 print(fractions.head())
 print(fractions.tail())
+
+
+### Computing percentage change in fraction of medals won
+
+# Apply the expanding mean: mean_fractions
+mean_fractions = fractions.expanding().mean()
+
+# Compute the percentage change: fractions_change
+fractions_change = mean_fractions.pct_change()*100
+
+# Reset the index of fractions_change: fractions_change
+fractions_change = fractions_change.reset_index()
+
+# Print first & last 5 rows of fractions_change
+print(fractions_change.head())
+print(fractions_change.tail())
