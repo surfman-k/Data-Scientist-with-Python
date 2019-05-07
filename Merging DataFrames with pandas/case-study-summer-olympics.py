f9c47007e990b39fcff064da6210f699d3953c94
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-05-06 22:25:16 -0400

    Reshaping and plotting

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -126,3 +126,29 @@ fractions_change = fractions_change.reset_index()
 # Print first & last 5 rows of fractions_change
 print(fractions_change.head())
 print(fractions_change.tail())
+
+
+############################
+## Reshaping and plotting ##
+############################
+
+# Import pandas
+import pandas as pd
+
+# Left join editions and ioc_codes: hosts
+hosts = pd.merge(editions, ioc_codes, how='left')
+
+# Extract relevant columns and set index: hosts
+hosts = hosts[['Edition','NOC']].set_index('Edition')
+
+# Fix missing 'NOC' values of hosts
+print(hosts.loc[hosts.NOC.isnull()])
+hosts.loc[1972, 'NOC'] = 'FRG'
+hosts.loc[1980, 'NOC'] = 'URS'
+hosts.loc[1988, 'NOC'] = 'KOR'
+
+# Reset Index of hosts: hosts
+hosts = hosts.reset_index()
+
+# Print hosts
+print(hosts)
