e0b9a6bee9c199af4daa71228ea03307c1569ee3
Konrad Kipczak <konrad.kipczak@gmail.com>
2019-05-07 22:28:06 -0400

    Plotting influence of host country

--- Merging DataFrames with pandas/case-study-summer-olympics.py
+++ Merging DataFrames with pandas/case-study-summer-olympics.py
@@ -190,3 +190,23 @@ influence = merged.set_index('Edition').sort_index()
 
 # Print first 5 rows of influence
 print(influence.head())
+
+
+### Plotting influence of host country
+
+# Import pyplot
+import matplotlib.pyplot as plt
+
+# Extract influence['Change']: change
+change = influence['Change']
+
+# Make bar plot of change: ax
+ax = change.plot(kind='bar')
+
+# Customize the plot to improve readability
+ax.set_ylabel("% Change of Host Country Medal Count")
+ax.set_title("Is there a Host Country Advantage?")
+ax.set_xticklabels(editions['City'])
+
+# Display the plot
+plt.show()
